506401c85a6bf9dde014dca75b223bf7d03d97c4
Diego Alexis DM <74570702+dieguinduartee@users.noreply.github.com>
2022-10-12 17:16:59 -0500

    Add files via upload

--- about.php
+++ about.php
@@ -7,20 +7,123 @@
         <div class="row">
 	        <!-- Page Content -->
 	        <div class="col-md-8">
-            <h1 class="page-header">Quienes Somos </h1>
+            <h1 class="page-header">MOVILIDAD INFORMACION </h1>
             <br>
-           <p>Formiik es la plataforma SaaS más robusta con soluciones de software para financieras.</p>
-           
-           <p>Ayudamos a bancos y microfinancieras a dar el paso hacia la era digital; construyendo soluciones que mejoran la gestión de procesos y responden a las necesidades tecnológicas del mercado financiero.</b>
-           
-           <br><br><br>
+            <div class="col-md-12">
+                    <div class="news-v3 bg-color-white col-md-12 col-sm-12 col-xs-12">
+                        <!-- video -->
+                        <div class="margin-bottom-20">
+                            
+                            
+                                                    </div>
+                                                <img class="img-responsive" src="https://ww2.ufps.edu.co/public/imagenes/noticia/3413ab4628109703ba7c7a004e3fb75d.jpg" alt="">
+                                                                    </div>
+                    <!-- End Imagen -->
+                    <div class="text-right">
+                        <em><h6 style="color:#aa1916;">
+                                <i class="fa fa-calendar"></i> Publicado el día
+                                jueves, 04 de noviembre del 2021 / No. 099                            </h6></em>
+                    </div>
+                    <div class="text-left">
+                                                <h5 class="parrafo_noticia"> <p>La Universidad Francisco de Paula Santander a través de la Oficina de Relaciones Interinstitucionales e Internacionales – ORII – dio apertura a la convocatoria para el Programa de Intercambio Académico Latinoamericano (PILA) para el primer semestre de 2022, en modalidad presencial y virtual.</p>
 
-           Desarrollado por: <br><br>
-           Name: Mauricio Sevilla <br>
-           Email: mauriciosb@kata-software.com <br>
-           Website: https://formiik.com <br>
-           <br>
-           <br>
+<p>En esta oportunidad y luego de año y medio de restricciones por consecuencia de la pandemia, la UFPS retoma las actividades de movilidad internacional saliente de estudiantes, cumpliendo las normas de bioseguridad y los acuerdos interinstitucionales en el marco de los protocolos establecidos para este proceso.</p>
+
+<p>Nelson Emilio García Torres, director de la ORII, explicó que para el caso de la movilidad presencial se tendrá la posibilidad de postular 4 estudiantes para que realicen su estancia académica en países como Argentina y México.</p>
+
+<p>“En Argentina logramos tener convenio con dos Instituciones Educativas, la Universidad Nacional del Nordeste y la Universidad Nacional del Sur, y en México tendremos la articulación académica con la Universidad Autónoma de Guerrero”, resaltó García Torres.</p>
+
+<p><strong><em>En cuanto a los requisitos para los estudiantes interesados en postularse a la movilidad presencial, el directivo UFPS subrayó los siguientes: </em></strong></p>
+
+<ul style="list-style-type:circle">
+	<li>Deberán contar con el esquema de vacunación completo contra el Covid-19.</li>
+	<li>Ser estudiante regular de la UFPS en uno de los Programas Académicos señalados en la convocatoria.</li>
+	<li>Cumplir con los requisitos internos de movilidad estudiantil internacional&nbsp;establecidos por la institución receptora y la UFPS.</li>
+	<li>Presentar un plan de estudios avalado por el Director del Programa Académico de las asignaturas que cursará, y de las asignaturas que le serán&nbsp;homologadas; mínimo tres para las Universidades de Argentina y mínimo&nbsp;cuatro para Universidades de México.</li>
+</ul>
+
+<p>En cuanto al escenario de la convocatoria de movilidad virtual, el director de la ORII manifestó que los estudiantes tendrán la posibilidad de cursar asignaturas en Instituciones de Educación Superior de países como Argentina, Brasil, Chile, Cuba, México, Nicaragua y Uruguay.&nbsp;</p>
+
+<p><strong><em>Para este proceso los interesados deberán identificar las posibilidades académicas (asignaturas disponibles) en las Universidades extranjeras a través de la página del Programa PILA: <a href="https://www.programapila.lat/">https://www.programapila.lat/</a></em></strong> ingresando al botón "Acceso a estudiantes - asignaturas para intercambio PILAVirtual".</p>
+
+<p>“Para la movilidad virtual, los estudiantes deberán comunicarse con la oficina de la ORII para postularlos como institución y tener claro los requisitos establecidos para este tipo de estancia académica mediada por las TIC”, informó el directivo UFPS.&nbsp;</p>
+</h5>
+                                            </div>
+
+                    <div id="fb-root" class=" fb_reset"><div style="position: absolute; top: -10000px; width: 0px; height: 0px;"><div></div></div></div>
+
+                    <!---- script redes social facebook ---->
+                    <script type="text/javascript">
+                        (function () {
+                            var element = document.createElement('script');
+                            element.type = "text/javascript";
+                            element.async = true;
+                            element.id = "facebook-jssdk"
+                            element.src = "//connect.facebook.net/es_ES/all.js#xfbml=1";
+                            var s = document.getElementsByTagName('script')[0];
+                            s.parentNode.insertBefore(element, s);
+                        })();
+                    </script>
+                    <!---- final script redes social facebook ---->
+
+                    <!-- <div class="fb-like" data-href="http://norfipc.com/pagina.html" data-width="292"
+                         data-send="true"></div> -->
+                </div>
+
+                <div class="row margin-top-60">
+    <h4 style="color:#aa1916;">Últimas Noticias</h4>
+            <div class="col-md-4">
+            <div class="blog-thumb blog-thumb-circle margin-bottom-15">
+                <div class="blog-thumb-hover">
+                    <img class="img-responsive" src="https://ww2.ufps.edu.co/public/imagenes/noticia/ec2296966cae4a8c0d09fc6982e6847f.jpg" alt="">
+                    <a class="hover-grad" href="https://ww2.ufps.edu.co/unoticia/especializacion-logisticaynegociosufps"><i class="fa fa-link"></i></a>
+                </div>
+                <div class="blog-thumb-desc">
+                    <h3>
+                        <a href="https://ww2.ufps.edu.co/unoticia/especializacion-logisticaynegociosufps">UFPS da inicio a la primera cohorte de la Especialización en Logística y Negocios Internacionales</a>
+                    </h3>
+                    <ul class="blog-thumb-info">
+                        <li><i class="fa fa-calendar"></i> lunes, 10 de octubre del 2022</li>
+
+                                                                                                                        </ul>
+                </div>
+            </div>
+        </div>
+            <div class="col-md-4">
+            <div class="blog-thumb blog-thumb-circle margin-bottom-15">
+                <div class="blog-thumb-hover">
+                    <img class="img-responsive" src="https://ww2.ufps.edu.co/public/imagenes/noticia/02dd8c5c888f9b8f75900ad941f0e3ce.jpg" alt="">
+                    <a class="hover-grad" href="https://ww2.ufps.edu.co/unoticia/buscandocarreara-ufps"><i class="fa fa-link"></i></a>
+                </div>
+                <div class="blog-thumb-desc">
+                    <h3>
+                        <a href="https://ww2.ufps.edu.co/unoticia/buscandocarreara-ufps">UFPS recibió a 109 colegios del área metropolitana de Cúcuta en la feria, “Buscando Carrera”  </a>
+                    </h3>
+                    <ul class="blog-thumb-info">
+                        <li><i class="fa fa-calendar"></i> jueves, 06 de octubre del 2022</li>
+
+                                                                                                                        </ul>
+                </div>
+            </div>
+        </div>
+            <div class="col-md-4">
+            <div class="blog-thumb blog-thumb-circle margin-bottom-15">
+                <div class="blog-thumb-hover">
+                    <img class="img-responsive" src="https://ww2.ufps.edu.co/public/imagenes/noticia/0d8322d9572fecac8eab6a300b0ef778.jpg" alt="">
+                    <a class="hover-grad" href="https://ww2.ufps.edu.co/unoticia/maraton-nacional-programacion-2022"><i class="fa fa-link"></i></a>
+                </div>
+                <div class="blog-thumb-desc">
+                    <h3>
+                        <a href="https://ww2.ufps.edu.co/unoticia/maraton-nacional-programacion-2022">La UFPS se destacó en Maratón Nacional de Programación</a>
+                    </h3>
+                    <ul class="blog-thumb-info">
+                        <li><i class="fa fa-calendar"></i> miércoles, 05 de octubre del 2022</li>
+
+                                                                                                                        </ul>
+                </div>
+            </div>
+        </div>
+    </div>
 
 
 
--- index.php
+++ index.php
@@ -4,6 +4,65 @@
         
    <?php include 'includes/navbar.php';?>
         
+<br>
+
+<br>
+<h1>MOVILIDAD</h1>
+
+
+<div class="container content-prin profile">
+
+        <div class="row margin-bottom-10 margin-top-10">
+            <div class="headline-center-v2 margin-bottom-10">
+                <h1 style="font-size: 30px; color:#fff;"><b>Novedades</b></h1>
+                <span class="bordered-icon"><i class="fa fa-files-o" aria-hidden="true"></i></span>
+            </div>
+                                                                                                    <div class="col-sm-4">
+                    <div class="service-block-v1 md-margin-bottom-50" style="background: #fff; border-top: 5px solid #f1c40f;">
+                                                    <i class="icon-custom icon-lg rounded-x icon-color-yellow icon-line fa fa-link" style="background: #fff;"></i>
+                                                                                                <h5 class="title-v3-bg text-uppercase"><a href="/udestacado/acta-eliminacion-admisiones" style="text-transform:none; color:#464646;"><b>Acta eliminación de documentos de Admisiones y Registro Académico</b></a>
+                        </h5>
+                        <p>
+                            viernes                            ,
+                            07                            octubre                            2022                        </p>
+                        <a href="/udestacado/acta-eliminacion-admisiones"><b>Leer
+                                más</b></a>
+                    </div>
+                </div>
+                                                                                                    <div class="col-sm-4">
+                    <div class="service-block-v1 md-margin-bottom-50" style="background: #fff; border-top: 5px solid #3498db;">
+                                                    <i class="icon-custom icon-lg rounded-x icon-color-blue icon-line fa fa-link" style="background: #fff;"></i>
+                                                                                                <h5 class="title-v3-bg text-uppercase"><a href="/udestacado/boletin_22_CE_22" style="text-transform:none; color:#464646;"><b>El Consejo Electoral de la Universidad Francisco de Paula Santander se permite informar a la comunidad universitaria que participará en la consulta democrática para la conformación de la lista de candidatos al cargo de Rector...</b></a>
+                        </h5>
+                        <p>
+                            jueves                            ,
+                            06                            octubre                            2022                        </p>
+                        <a href="/udestacado/boletin_22_CE_22"><b>Leer
+                                más</b></a>
+                    </div>
+                </div>
+                                                                                                    <div class="col-sm-4">
+                    <div class="service-block-v1 md-margin-bottom-50" style="background: #fff; border-top: 5px solid #e74c3c;">
+                                                    <i class="icon-custom icon-lg rounded-x icon-color-red icon-line fa fa-link" style="background: #fff;"></i>
+                                                                                                <h5 class="title-v3-bg text-uppercase"><a href="/udestacado/suspencion-terminos-administrativos-30septiembre" style="text-transform:none; color:#464646;"><b>Resolución 1775 del 27 de septiembre de 2022. Por la cual se suspenden términos administrativos. </b></a>
+                        </h5>
+                        <p>
+                            miércoles                            ,
+                            28                            septiembre                            2022                        </p>
+                        <a href="/udestacado/suspencion-terminos-administrativos-30septiembre"><b>Leer
+                                más</b></a>
+                    </div>
+                </div>
+                        <div class="col-md-12 margin-top-10">
+                <a href="/udestacado/listado_destacados/" class="btn-u btn-brd btn-brd-hover btn-u-light btn-u-sm pull-right tooltips" data-toggle="tooltip" data-placement="left" data-original-title="Más Novedades Anteriores">Ver
+                    más <i class="fa fa-chevron-circle-right" aria-hidden="true"></i></a>
+
+            </div>
+        </div>
+        <!--/row-->
+
+    </div>
+
 
     <div class="container">
         <div class="row">
@@ -46,6 +105,8 @@ while ($row = mysqli_fetch_assoc($run_query)) {
           <li class="next"><a href="#">Más nuevos <span class="glyphicon glyphicon-arrow-right"></span></a></li>
         </ul>
           </div>
+
+
 	        
 	        <div class="col-md-4">
 
