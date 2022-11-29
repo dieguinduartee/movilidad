c2b5d48b8db58e4968ea305e96134cbc500516b5
Luis Vicente Albarracin <albarracinluisvicente@gmail.com>
2022-11-29 11:25:27 -0500

    footer externo

--- includes/footer.php
+++ includes/footer.php
@@ -1,8 +1,7 @@
-<footer class="bg-light text-center text-lg-start navbar-fixed-bottom mt-1 pt-1">
-	<!-- Copyright -->
-	<div class="text-center p-3">
-		© 2022 Copyright: <span class="text-dark">Ingeniería de Software
-			UFPS</span>
-	</div>
-	<!-- Copyright -->
+<footer class="foo">
+    <div class="row">
+        <div class="col-lg-12">
+            <p style="text-align:center; font-size:17px;"><b >Ingeniería de Software 2022</b></p>
+        </div>
+    </div>
 </footer>
